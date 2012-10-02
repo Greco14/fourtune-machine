f89a4f2876a7c36993cb41eee06530763b25a7d4
R. Diaz de Leon <leon@elinter.net>
2012-10-02 13:44:28 -0500

    Updated the server and added a sample client for UDP

--- udp_server.py
+++ udp_server.py
@@ -3,15 +3,17 @@ import SocketServer
 class CheckinCounter(SocketServer.BaseRequestHandler):
 
     def handle(self):
-        status = 0
+	print "Got Request"
+        status = '0'
         data = self.request[0].strip()
 	if (data == 'c'):
-	    status = 1
+	    status = '1'
         socket = self.request[1]
+	print "Got data: " + status
         socket.sendto(status, self.client_address)
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 2323
+    HOST, PORT = "66.228.50.204", 2323
     print "Starting server..."
     server = SocketServer.UDPServer((HOST, PORT), CheckinCounter)
     server.serve_forever()
