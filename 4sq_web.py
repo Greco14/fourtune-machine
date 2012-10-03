8a2abf067ba0e893073f0ea84409fb61f85e33d2
R. Diaz de Leon <leon@elinter.net>
2012-10-02 20:36:18 -0500

    improved the foursquare push server

--- 4sq_web.py
+++ 4sq_web.py
@@ -1,40 +1,31 @@
-import socket, os
-from SocketServer import BaseServer
-from BaseHTTPServer import HTTPServer
-from SimpleHTTPServer import SimpleHTTPRequestHandler
-from OpenSSL import SSL
+import web
+from web.wsgiserver import CherryPyWSGIServer
+from collections import defaultdict
 
-FOUR
-SQ_SECRET = ''
+import json
 
-class SecureHTTPServer(HTTPServer):
-    def __init__(self, server_address, HandlerClass):
-	BaseServer.__init__(self, server_address, HandlerClass)
-	ctx = SSL.Context(SSL.SSLv23_METHOD)
-        # server.pem's location (containing the server private key and
-        # the server certificate).
-	fpem = '/tmp/server.pem'
-	ctx.use_privatekey_file (fpem)
-	ctx.use_certificate_file(fpem)
-	self.socket = SSL.Connection(ctx, socket.socket(self.address_family,
-			self.socket_type))
-	self.server_bind()
-	self.server_activate()
+CherryPyWSGIServer.ssl_certificate = "/tmp/cacert.pem"
+CherryPyWSGIServer.ssl_private_key = "/tmp/server.pem"
 
+DEBUG=0
 
-class SecureHTTPRequestHandler(SimpleHTTPRequestHandler):
-    def setup(self):
-        self.connection = self.request
-	self.rfile = socket._fileobject(self.request, "rb", self.rbufsize)
-	self.wfile = socket._fileobject(self.request, "wb", self.wbufsize)
+cis = defaultdict(int);
 
-def recvCheckIn(HandlerClass = SecureHTTPRequestHandler,
-				ServerClass = SecureHTTPServer):
-    server_address = ('', 443) # (address, port)
-    httpd = ServerClass(server_address, HandlerClass)
-    sa = httpd.socket.getsockname()
-    print "Serving HTTPS on", sa[0], "port", sa[1], "..."
-    httpd.serve_forever()
+urls = ("/.*", "hello")
+app = web.application(urls, globals())
 
-if __name__ == '__main__':
-    recvCheckIn()
+class hello:
+    def GET(self):
+        return 'Hello, world!'
+
+    def POST(self):
+    	input = web.input()
+	form = json.loads(web.data())
+	if form['type'] == 'checkin':
+	    uid = 'user' + str(form['user']['id'])
+	    cis[uid] += 1
+	    return "Count: " + str(cis[uid])
+        return 'Unknown command' 
+
+if __name__ == "__main__":
+    app.run()
