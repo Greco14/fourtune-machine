1c3e8031f49f032837614bd2f366d4870c7fe632
R. Diaz de Leon <leon@elinter.net>
2012-10-09 23:59:07 -0500

    Fixed the urlencode() function call

--- 4sq_auth.py
+++ 4sq_auth.py
@@ -17,23 +17,30 @@ app = web.application(urls, globals())
 
 class redirect:
     def GET(self):
-        query = '?client_id={1}&response_type=code&redirect_uri={2}'
-        query = query.format(USER_ID, urlencode(MYURL))
-        raise web.redirect('https://foursquare.com/oauth2/authenticate' + query)
+	query = urlencode((
+	    ('client_id', USER_ID),
+	    ('response_type', 'code'),
+	    ('redirect_uri', MYURL)
+	    ))
+        raise web.redirect(
+	    'https://foursquare.com/oauth2/authenticate?' + query,
+            '301 Moved Permanently'
+            )
 
 class callback:
     def GET(self):
         # validation code
         query = web.input(code=None,fsqCallback=None)
         if query.code:
-            req_query = '?client_id={1}'\
-                '&client_secret={2}'\
-                '&grant_type=authorization_code'\
-                '&redirect_uri={3}'\
-                '&code={4}'
-            url = urlencode(MYURL)
-            qstring = req_query.format(USER_ID, SECRET, url, query.code)
-            res = urlopen(ENDPOINT + qstring)
+            query = urlencode((
+		('client_id', USER_ID),
+		('client_secret', SECRET),
+		('grant_type', 'authorization_code'),
+		('redirect_uri', MYURL),
+		('code', query.code)
+		))
+            res = urlopen(ENDPOINT +'?'+ query)
+	    print res
         else:
             return 'Missing code parameter';
 
